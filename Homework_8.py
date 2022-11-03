394a98566bbffc9e88c19263ef992d6379272b9c
VictoriiaCher <112928478+VictoriiaCher@users.noreply.github.com>
2022-11-03 23:36:14 +0200

    Враховані всі зауваження ментора
    1. Докстрінги перенесені в тіло функцій
    2. L13-L16 виведено в окрему функцію

--- Homework_8.py
+++ Homework_8.py
@@ -10,38 +10,42 @@ def main(users: dict):
     for name, birthday in users.items():
         delta = birthday - datetime.now()
         if int(delta.days) in range(0, 6):
-            if birthday.strftime("%A") == "Saturday":
-                birthday += timedelta(days=2)
-            elif birthday.strftime("%A") == "Sunday":
-                birthday += timedelta(days=1)
+            birthday_on_weekend(birthday)
             birthday_list[birthday.strftime("%A")].append(name)
     output_birthday_list(birthday_list)
 
 
-"""Функція виконує приведення дати народження до дня народження у поточному році """
-
-
 def transform_date(users: dict) -> dict:
+    """Функція виконує приведення дати народження до дня народження у поточному році"""
+
     current_year = (datetime.now()).year
     for name, birthday in users.items():
         users.update({name: birthday.replace(year=current_year)})
     return users
 
 
-"""Функція виводить на екран список колег, яких треба привітати на тижні"""
+def birthday_on_weekend(birthday: datetime) -> datetime:
+    """Функція оброблює дні народження, які припадають на вихідний день"""
+
+    if birthday.strftime("%A") == "Saturday":
+        birthday += timedelta(days=2)
+    elif birthday.strftime("%A") == "Sunday":
+        birthday += timedelta(days=1)
+    return birthday
 
 
 def output_birthday_list(birthday_list: dict):
+    """Функція виводить на екран список колег, яких треба привітати на тижні"""
+
     for day, name in birthday_list.items():
         name = ", ".join(name)
         print(f"{day}: {name}")
 
 
-"""Функція виконує читання файлу з вихідними даними та приводить отримані дані 
-до словника виляду "Ім'я: Дата Народження (формат YYYY-MM-D)" """
-
-
 def read_birthday_list(source: str) -> dict:
+    """Функція виконує читання файлу з вихідними даними та приводить отримані дані
+    до словника виляду "Ім'я: Дата Народження (формат YYYY-MM-D)" """
+
     with open(source, "r") as file:
         users = {}
         for line in file.readlines():
